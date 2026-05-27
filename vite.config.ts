a0e68d1d1a1c5616b999e4c5df557f676fb0132f
Nicholaus Mayes <113538642+NicholausMayes@users.noreply.github.com>
2026-05-26 20:40:43 -0700

    Update base path based on GITHUB_ACTIONS

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/TorresPropertyServices/',
+  base: process.env.GITHUB_ACTIONS ? '/TorresPropertyServices/' : '/',
   plugins: [
     figmaAssetResolver(),
     react(),
